ba21175c336d37a9153d8afd5ce4ec292ee0c24d
fahu26 <fahimshaikh2606@gmail.com>
2019-08-08 00:35:35 +0530

    modified code by removing arrayList and used array instead

--- Searching_Array/src/main/java/comm/fahim/SearchingNumber.java
+++ Searching_Array/src/main/java/comm/fahim/SearchingNumber.java
@@ -1,36 +1,15 @@
 package comm.fahim;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SearchingNumber {
-	public boolean searchNumber(int number)
-	{
-		boolean isFound=false;
-		List<Integer> numbers=new ArrayList<Integer>();
-		numbers.add(5);
-		numbers.add(12);
-		numbers.add(14);
-		numbers.add(6);
-		numbers.add(78);
-		numbers.add(19);
-		numbers.add(1);
-		numbers.add(23);
-		numbers.add(26);
-		numbers.add(35);
-		numbers.add(37);
-		numbers.add(7);
-		numbers.add(52);
-		numbers.add(86);
-		numbers.add(47);
-		if(numbers.contains(number))
-		{
-			System.out.println(number+" is present in the ArrayList");
-			isFound=true;
-		}
-		else {
-			System.out.println(number+" is not present in the ArrayList");
-			isFound=false;
+	public boolean searchNumber(int number) {
+		boolean isFound = false;
+		int[] numbers = { 5, 12, 14, 6, 78, 19, 1, 23, 26, 35, 37, 7, 52, 86, 47 };
+		for (int number2 : numbers) {
+			if (number2 == number) {
+				System.out.println(number + " is present in the Array");
+				isFound = true;
+				break;
+			}
 		}
 		return isFound;
 	}
--- Searching_Array/src/test/java/comm/fahim/SearchingNumberTest.java
+++ Searching_Array/src/test/java/comm/fahim/SearchingNumberTest.java
@@ -7,30 +7,31 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SearchingNumberTest {
+		public static SearchingNumber searchNumber;
+		@BeforeClass
+		public static void setUpBeforeClass() throws Exception {
+			searchNumber=new SearchingNumber();
+		}
 
-	static SearchingNumber searchNumber;
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		searchNumber=new SearchingNumber();
-	}
+		@Test
+		public void testIfNumberPresentInArrayList_True() {
+			assertTrue(searchNumber.searchNumber(47));
+		}
+		
+		@Test
+		public void testIfNumberPresentInArrayList_False() {
+			assertFalse(searchNumber.searchNumber(0));
+		}
+		
+		@Test
+		public void testIfNumberPresentInArrayList_NegativeNumber() {
+			assertFalse(searchNumber.searchNumber(-47));
+		}
+		
+		@AfterClass
+		public static void tearDownAfterClass() throws Exception {
+			searchNumber=null;
+		}
 
-	@Test
-	public void testIfNumberPresentInArrayList_True() {
-		assertTrue(searchNumber.searchNumber(47));
-	}
-	
-	@Test
-	public void testIfNumberPresentInArrayList_False() {
-		assertFalse(searchNumber.searchNumber(0));
-	}
-	
-	@Test
-	public void testIfNumberPresentInArrayList_NegativeNumber() {
-		assertFalse(searchNumber.searchNumber(-47));
-	}
-	
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		searchNumber=null;
-	}
+
 }
