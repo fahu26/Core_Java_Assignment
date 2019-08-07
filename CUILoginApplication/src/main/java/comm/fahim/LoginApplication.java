7724442d25ff1c9ca46211dfe8f015a6b8d40888
fahu26 <fahimshaikh2606@gmail.com>
2019-08-07 23:57:39 +0530

    Removed main method

--- CUILoginApplication/src/main/java/comm/fahim/LoginApplication.java
+++ CUILoginApplication/src/main/java/comm/fahim/LoginApplication.java
@@ -1,6 +1,5 @@
 package comm.fahim;
 
-import java.util.Scanner;
 
 public class LoginApplication {
 	public Boolean Login(String userName,String passWord) {
@@ -15,28 +14,17 @@ public class LoginApplication {
 		}
 		return isvalid;
 	}
-	public static void main(String args[]) {
-		LoginApplication application=new LoginApplication();
-		Scanner scanner=new Scanner(System.in);
-		int countLoginFailed=0;
-		for(int i=0;i<=countLoginFailed;i++) {
-			if(countLoginFailed>=0 && countLoginFailed<3){
-		System.out.println("Enter Your UserName: ");
-		String userName=scanner.nextLine();
-		System.out.println("Enter Your Password: ");
-		String passWord=scanner.nextLine();
-		boolean result=application.Login(userName, passWord);
-		if(result) {
-			System.out.println("Welcome "+userName);
-			scanner.close();
-			break;
-		}
-		else {
-			countLoginFailed++;
-		}}
-			else {
-				System.out.println("Contact Admin");
-				scanner.close();
-			}
-			}
-}}
+	/*
+	 * public static void main(String args[]) { LoginApplication application=new
+	 * LoginApplication(); Scanner scanner=new Scanner(System.in); int
+	 * countLoginFailed=0; for(int i=0;i<=countLoginFailed;i++) {
+	 * if(countLoginFailed>=0 && countLoginFailed<3){
+	 * System.out.println("Enter Your UserName: "); String
+	 * userName=scanner.nextLine(); System.out.println("Enter Your Password: ");
+	 * String passWord=scanner.nextLine(); boolean
+	 * result=application.Login(userName, passWord); if(result) {
+	 * System.out.println("Welcome "+userName); scanner.close(); break; } else {
+	 * countLoginFailed++; }} else { System.out.println("Contact Admin");
+	 * scanner.close(); } } }
+	 */
+	}
